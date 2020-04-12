2108ce772bea2ed30798a8c61014f8ab22c50334
HeoliaJdR <admin@heolia.eu>
2020-04-12 16:57:36 +0200

    Basic Sprint

--- Assets/Scripts/Physical/BasicMovement.cs
+++ Assets/Scripts/Physical/BasicMovement.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Physical
 {
     public class BasicMovement : MonoBehaviour
     {
         [SerializeField] private CharacterController controller;
-        [SerializeField] private float speed;
+        [SerializeField] private float speedExposer;
         [SerializeField] private float jumpHeight;
         [SerializeField] private float gravity; 
         [SerializeField] private Transform groundCheck;
@@ -14,7 +15,13 @@ namespace Physical
     
         private Vector3 _velocity;
         private float _minusVelocity = -2f;
+        private float _speed;
         private bool _isGroundCheck;
+
+        private void Start()
+        {
+            _speed = speedExposer;
+        }
 
         // Update is called once per frame
         void Update()
@@ -25,19 +32,26 @@ namespace Physical
             {
                 _velocity.y = _minusVelocity;
             }
+            
         
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
             Vector3 movement = transform.right * x + transform.forward * z;
-            controller.Move(movement * (speed * Time.deltaTime));
+            if (Input.GetButton("Run") && _isGroundCheck)
+            {
+                _speed = speedExposer * 2f;
+            }
+            else
+            {
+                _speed = speedExposer;
+            }
+            controller.Move(movement * (_speed * Time.deltaTime));
             if (Input.GetButtonDown("Jump") && _isGroundCheck)
             {
-                Debug.Log("Je saute zebi");
-                _velocity.y = Mathf.Sqrt(jumpHeight * _minusVelocity * gravity);
+                _velocity.y = Mathf.Sqrt(jumpHeight * (_minusVelocity - 1) * gravity);
             }
             _velocity.y += gravity * Time.deltaTime;
-
             controller.Move(_velocity * Time.deltaTime);
         }
     }
