dc94989b91d069ef1f731e4cafc82ba4bc22d1fc
HeoliaJdR <admin@heolia.eu>
2020-04-12 15:19:52 +0200

    Fix namespace

--- Assets/Scripts/Physical/BasicMovement.cs
+++ Assets/Scripts/Physical/BasicMovement.cs
@@ -1,43 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BasicMovement : MonoBehaviour
+namespace Physical
 {
-    [SerializeField] private CharacterController controller;
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpHeight;
-    [SerializeField] private float gravity; 
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundDistance;
-    [SerializeField] private LayerMask groundMask;
-    
-    private Vector3 _velocity;
-    private float _minusVelocity = -2f;
-    private bool _isGroundCheck;
-
-    // Update is called once per frame
-    void Update()
+    public class BasicMovement : MonoBehaviour
     {
-        _isGroundCheck = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        [SerializeField] private CharacterController controller;
+        [SerializeField] private float speed;
+        [SerializeField] private float jumpHeight;
+        [SerializeField] private float gravity; 
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private float groundDistance;
+        [SerializeField] private LayerMask groundMask;
+    
+        private Vector3 _velocity;
+        private float _minusVelocity = -2f;
+        private bool _isGroundCheck;
 
-        if (_isGroundCheck && _velocity.y < 0f)
+        // Update is called once per frame
+        void Update()
         {
-            _velocity.y = _minusVelocity;
-        }
+            _isGroundCheck = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (_isGroundCheck && _velocity.y < 0f)
+            {
+                _velocity.y = _minusVelocity;
+            }
         
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        Vector3 movement = transform.right * x + transform.forward * z;
-        controller.Move(movement * (speed * Time.deltaTime));
-        if (Input.GetButtonDown("Jump") && _isGroundCheck)
-        {
-            Debug.Log("Je saute zebi");
-            _velocity.y = Mathf.Sqrt(jumpHeight * _minusVelocity * gravity);
+            Vector3 movement = transform.right * x + transform.forward * z;
+            controller.Move(movement * (speed * Time.deltaTime));
+            if (Input.GetButtonDown("Jump") && _isGroundCheck)
+            {
+                Debug.Log("Je saute zebi");
+                _velocity.y = Mathf.Sqrt(jumpHeight * _minusVelocity * gravity);
+            }
+            _velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(_velocity * Time.deltaTime);
         }
-        _velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(_velocity * Time.deltaTime);
     }
 }
