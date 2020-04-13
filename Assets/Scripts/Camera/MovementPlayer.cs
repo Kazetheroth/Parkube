90944ed07db6cba354763ae71f217eccb5aa45d0
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2020-04-13 12:03:18 +0200

    Movement camera and speed feeling

---
+++ Assets/Scripts/Camera/MovementPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform end;
+    [SerializeField] private float speed;
+    [SerializeField] private RadialBlur radialBlur;
+
+    private float _counterIdle;
+    private float _counterRun;
+    private float _counterSpeed;
+    private bool _switchIdle;
+    private bool _switchRun;
+    private bool _onStart;
+    
+    private void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.Z))
+        {
+            UpdateCounter(ref _switchRun, ref _counterRun);
+            _counterIdle = 0;
+            _counterSpeed += Time.deltaTime;
+            cameraTransform.rotation = Quaternion.Slerp(start.rotation, end.rotation, _counterRun);
+            playerTransform.Translate(speed * _counterSpeed * Time.deltaTime * Vector3.forward );
+            
+        }
+        else
+        {
+            _counterSpeed -= Time.deltaTime * 4;
+            _onStart = false;
+            _counterRun = 0;
+            UpdateCounter(ref _switchIdle, ref _counterIdle);
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, start.rotation,
+                _counterIdle);
+        }
+        _counterSpeed = Mathf.Clamp(_counterSpeed, 0, 1);
+        radialBlur.blurStrength = _counterSpeed * 1.5f;
+    }
+
+    
+    private void UpdateCounter(ref bool inverse,ref float counter)
+    {
+        if (counter > 1)
+            inverse = true;
+        else if (counter < 0) 
+            inverse = false;
+        
+
+        if (inverse)
+            counter -= Time.deltaTime * 4;
+        else
+            counter += Time.deltaTime * 4;
+
+    }
+}
